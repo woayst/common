c0999e0608fbe024d198e1db141c75c0a0993de2
unknown <phannhan6000@gmail.com>
2021-09-05 14:26:48 +0700

    update mission.js

--- js/mission.js
+++ js/mission.js
@@ -402,26 +402,3 @@ function updateMyPoint() {
             $('.your-point').html(data.sum);
         })
 }
-
-var tabLinks = document.querySelectorAll(".tablinks");
-var tabContent = document.querySelectorAll(".tabcontent");
-
-tabLinks.forEach(function (el) {
-    el.addEventListener("click", openTabs);
-});
-function openTabs(el) {
-    var btn = el.currentTarget; // lắng nghe sự kiện và hiển thị các element
-    var electronic = btn.dataset.target; // lấy giá trị trong data-electronic
-
-    tabContent.forEach(function (el) {
-        el.classList.remove("active");
-    });
-
-    tabLinks.forEach(function (el) {
-        el.classList.remove("active");
-    });
-
-    document.querySelector("#" + electronic).classList.add("active");
-
-    btn.classList.add("active");
-}
