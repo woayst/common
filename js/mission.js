cf4e8eb182744befc50c2c81009d17eb7b29ecc7
unknown <phannhan6000@gmail.com>
2021-09-08 11:38:00 +0700

    fix  phan check spinning

--- js/mission.js
+++ js/mission.js
@@ -110,7 +110,7 @@ client.eventBus.on('login-done', function () {
                         $(".title-question").html(question.question);
                     }
                     $(document).on('click', '.btn-show-quiz', function () {
-                        if (client.checkSpinning()) return;
+                        // if (client.checkSpinning()) return;
                         console.log('click btn mission', {
                             current_question: current_question,
                             questions: questions
@@ -172,7 +172,7 @@ function missionComplete(name, new_quantity) {
         return;
     }
 
-    if (client.checkSpinning()) return;
+    // if (client.checkSpinning()) return;
 
     var player_game_id = client.user.get().player_game_id;
     client.mission.complete(name, player_game_id, new_quantity)
@@ -259,12 +259,12 @@ function processGoldHourMission() {
 }
 
 $(document).on("click", '.btn-invite-friend', function () {
-    if (client.checkSpinning()) return;
+    // if (client.checkSpinning()) return;
     MicroModal.show('w-share');
 })
 
 $(document).on("click", '.btn-share-fb', function () {
-    if (client.checkSpinning()) return;
+    // if (client.checkSpinning()) return;
     FB.ui({
         method: 'share',
         href: getShareLink(),
@@ -280,7 +280,7 @@ $(document).on("click", '.my-copy-link-btn', function () {
 })
 
 $(document).on('click', '.btn-show-qrcode', function () {
-    if (client.checkSpinning()) return;
+    // if (client.checkSpinning()) return;
     checkQrCode();
 })
 
